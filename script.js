1548f97c2b00bad7df6f6378d8bce38cb0ec32e8
TAYYAB2712 <123016957+TAYYAB2712@users.noreply.github.com>
2024-11-28 00:51:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simple animation example:
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+    section.addEventListener('mouseenter', () => {
+        section.style.transform = 'scale(1.05)';
+    });
+
+    section.addEventListener('mouseleave', () => {
+        section.style.transform = 'scale(1)';
+    });
+});
